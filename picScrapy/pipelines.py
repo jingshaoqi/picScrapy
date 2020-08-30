3d6127dd26d7d71f01b848648345fa6ae141b70b
chencichang <cichang.chen@verycloud.cn>
2020-08-30 11:11:16 +0800

    optimize pipeline code

--- picScrapy/pipelines.py
+++ picScrapy/pipelines.py
@@ -13,8 +13,8 @@ from scrapy import Request
 class PicscrapyPipeline(ImagesPipeline):
     def get_media_requests(self, item, info):
         # 通过meta属性传递title
-        return [Request(x, meta={'title': item['title'], 'cat': item['category_name']}) for x in
-                item.get(self.images_urls_field, [])]
+        meta = {'title': item['title'], 'cat': item['category_name']}
+        return [Request(url=x, meta=meta) for x in item.get(self.images_urls_field, [])]
 
     # 重写函数，修改了下载图片名称的生成规则
     def file_path(self, request, response=None, info=None):
